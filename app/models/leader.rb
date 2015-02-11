4879adc57c75e7d3087275992ae1c680bec2806a
dmcbrayer <dmcbrayer@gmail.com>
2015-02-11 00:50:06 -0500

    Ran annotate again.

--- app/models/leader.rb
+++ app/models/leader.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: leaders
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class Leader < ActiveRecord::Base
 	validates :name, presence: true
 
--- spec/models/leader_spec.rb
+++ spec/models/leader_spec.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: leaders
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 require 'rails_helper'
 
 RSpec.describe Leader, :type => :model do
