65873855185eddc350305b9c2f14c09e7fab7eac
JT <jtjobe@gmail.com>
2015-03-31 14:52:24 -0400

    Add Jordan's Javascript code to Stripe checkout form

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 Rails.configuration.stripe = {
-  :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
-  :secret_key      => ENV['STRIPE_SECRET_KEY']
+  :publishable_key => 'pk_test_To6xtnnLr2Ns3JUAwb5RkDK8',
+  :secret_key      => 'sk_test_HJ91GiwLBtPIB0XCF7udse0k'
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
