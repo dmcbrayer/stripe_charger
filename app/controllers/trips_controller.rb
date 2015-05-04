e8f9c56e2f71124d7c4f03e64d08b493e73dfe4e
dmcbrayer <dmcbrayer@gmail.com>
2015-05-03 23:57:42 -0400

    Added some fixes to ensure that an admin user can create a private trip.

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -2,7 +2,7 @@ class TripsController < ApplicationController
   before_action :set_trip, only: [:show, :edit, :update, :destroy]
 
   def index
-    @trips = Trip.paginate(:page => params[:page], :per_page => 3)
+    @trips = Trip.where(:private_trip => false).paginate(:page => params[:page], :per_page => 20)
   end
 
   def show
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -22,7 +22,6 @@ class Trip < ActiveRecord::Base
 	validates :leader, presence: true
 	validates :price, presence: true
 	
-  default_scope -> { where( 'private_trip = ?', false).all}
   default_scope -> { where( 'start_date > ?', Date.today - 1).all}
   default_scope -> { order(:start_date => :asc)}
 
